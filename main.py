b812163e22c687febd2dd8b1bdfa7a479b651159
The-Autonomous-Ali <sameerup1996@gmail.com>
2024-04-21 20:40:21 +0530

    Data_ingestion & Logs Completed

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-
 from mlProject.logging import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is our custome logs")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<\n\nx======")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/mlProject/pipeline/stage_01_data_ingestion.py
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -12,10 +12,10 @@ class DataIngestionTrainingPipeline:
 
     def main(self):
         config= ConfigurationManager()
-    data_Ingestion_config = config.get_data_ingestion_config()
-    data_ingestion = DataIngestion(config=data_Ingestion_config)
-    data_ingestion.download_file()
-    
+        data_Ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_Ingestion_config)
+        data_ingestion.download_file()
+        
 
 
 if __name__ =='__main__':
