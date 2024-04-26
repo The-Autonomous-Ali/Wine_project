c427382fd1b00afe77cf423f13e7d040d6c1d5c0
The-Autonomous-Ali <sameerup1996@gmail.com>
2024-04-26 21:40:09 +0530

    Model Evaluation Completed

--- src/mlProject/pipeline/stage_04_model_trainer.py
+++ src/mlProject/pipeline/stage_04_model_trainer.py
@@ -18,3 +18,16 @@ class ModelTrainerPipeline:
         model_trainer_config = config.get_model_trainer_config()
         model_trainer = ModelTrainer(config=model_trainer_config)
         model_trainer.train()
+
+
+
+if __name__ =="__main__":
+    try:
+        logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = ModelTrainerPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
